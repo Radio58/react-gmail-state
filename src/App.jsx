d42afac624cdf96a6471bfb4acef1fa0ab2b48d8
Radio58 <rt58gm@gmail.com>
2023-10-30 17:32:56 +0000

    started listing emails

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import Header from './components/header'
-import initialEmails from './data/emails'
+import { useState } from "react";
+import Header from "./components/header";
+import initialEmails from "./data/emails";
 
-import './styles/App.css'
+import "./styles/App.css";
 
 function App() {
-  // Use initialEmails for state
+  const [emails, setEmails] = useState(initialEmails)
+
   console.log(initialEmails)
+
+  const displayEmails = emails.map((email) => (
+    <li key={email.id} className="email">
+    <div className="select">
+      <input className="select-checkbox" type="checkbox" checked={email.read}/>
+    </div>
+    <div className="star">
+      <input className="star-checkbox" type="checkbox" checked={email.starred}/>
+    </div>
+    <div className="sender">{email.sender}</div>
+    <div className="title">{email.title}</div>
+  </li>
+  ))
 
   return (
     <div className="app">
@@ -21,7 +36,7 @@ function App() {
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+            onClick={() => {}}
           >
             <span className="label">Starred</span>
             <span className="count">?</span>
@@ -38,9 +53,12 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">
+        {displayEmails}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
